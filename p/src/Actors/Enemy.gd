4acca4e043c3c0a2a0bde81e90f06ca43c1a4383
Ivan <3hitemouse@gmail.com>
2021-05-04 00:46:43 +0300

    added player/enemy logic

---
+++ p/src/Actors/Enemy.gd
@@ -0,0 +1,23 @@
+extends "res://src/Actors/Actor.gd"
+
+
+onready var _timer := Timer.new()
+
+
+func _ready():
+	_velocity.x = -speed.x
+	
+	self.add_child(_timer)
+	_timer.connect("timeout", self, "queue_free")
+	_timer.set_wait_time(10)
+	if _timer.time_left <= 0:
+		_timer.start()
+
+
+func _on_BulletDetector_body_entered(body):
+	get_node("CollisionShape2D").disabled = true
+	queue_free()
+
+
+func _physics_process(delta):
+	_velocity = move_and_slide(_velocity, Vector2.UP)
