b1f096ece5a3e40b2c82ff66b3bac5e75bb9fbbb
Ivan <3hitemouse@gmail.com>
2021-05-10 04:42:48 +0300

    v1

--- p/src/Actors/Coin.gd
+++ p/src/Actors/Coin.gd
@@ -5,21 +5,26 @@ export var lifetime: float
 
 var velocity := Vector2.ZERO
 
+
 func start(_position, _direction):
 	position = _position
 	rotation = _direction.angle()
 	$Lifetime.wait_time = lifetime
 	velocity = _direction * speed
 	
+
 func _process(delta):
 	position += velocity * delta
+
 
 func _on_Coin_body_entered(_body):
 	if _body:
 		_body.queue_free()
 		ScoreRecord.score += 1
 
+	$PopSFX.play()
 	queue_free()
+
 
 func _on_Lifetime_timeout():
 	queue_free()
--- p/src/Levels/LevelTemplate.gd
+++ p/src/Levels/LevelTemplate.gd
@@ -4,17 +4,19 @@ export (PackedScene) var mob_scene
 
 func _ready():
 	randomize()
+	$Music.play()
 
 
 func _on_Player_shoot(bullet, _position, _direction):
 	var result = bullet.instance()
 	add_child(result)
 	result.start(_position, _direction)
+	$ShootSFX.play()
 
 
 func _on_MobTimer_timeout():
 	var location = $MobPath/MobSpawnLocation
-	location.unit_offset = rand_range(0.125, 0.88)
+	location.unit_offset = rand_range(0.125, 0.71)
 	
 	var mob = mob_scene.instance()
 	add_child(mob)
