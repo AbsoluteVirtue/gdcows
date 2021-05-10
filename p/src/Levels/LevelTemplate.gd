c02f3d79e819062345098da7f481cb0e62437d1d
Ivan <3hitemouse@gmail.com>
2021-05-10 03:31:19 +0300

    new enemy impl

--- p/src/Levels/LevelTemplate.gd
+++ p/src/Levels/LevelTemplate.gd
@@ -1,7 +1,24 @@
 extends Node2D
 
+export (PackedScene) var mob_scene
+
+func _ready():
+	randomize()
+
 
 func _on_Player_shoot(bullet, _position, _direction):
 	var result = bullet.instance()
 	add_child(result)
 	result.start(_position, _direction)
+
+
+func _on_MobTimer_timeout():
+	var location = $MobPath/MobSpawnLocation
+	location.unit_offset = rand_range(0.125, 0.88)
+	
+	var mob = mob_scene.instance()
+	add_child(mob)
+	mob.position = location.position
+	
+	var velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
+	mob.linear_velocity = velocity.rotated(location.rotation + PI / 2)
